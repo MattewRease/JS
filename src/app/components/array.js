90f40ba86fc37173252675683faa5960fcc76848
Dmytro Zhyrov <dzhyrov@actum.cz>
2017-12-19 14:32:53 +0100

    my var

--- src/app/components/array.js
+++ src/app/components/array.js
@@ -16,37 +16,43 @@ const people = ['Beck, Glenn', 'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick
 
 // Array.prototype.filter()
 // 1. Filter the list of inventors for those who were born in the 1500's
-const fifteen = inventors.filter(inventor => inventor.year >= 1500 && inventor.year <= 1599);
-// console.table(fifteen);
 
+const fifth = inventors.filter(inventor => (inventor.year >= 1500 && inventor.year <= 1600));
+// console.table(fifth);
 
 // Array.prototype.map()
 // 2. Give us an array of the inventors' first and last names
-const fullNames = inventors.map(inventor => `${inventor.first} ${inventor.last}`);
-// console.log(fullNames);
+
+const list = inventors.map(inventors => `${inventors.first} ${inventors.passed}`);
+// console.table(list);
 
 // Array.prototype.sort()
 // 3. Sort the inventors by birthdate, oldest to youngest
-const ordered = inventors.sort((a, b) => a.year > b.year ? 1 : -1);
+
+const ordered = inventors.sort((a, b) => {
+    return a.year - b.year;
+});
+
 // console.table(ordered);
 
 // Array.prototype.reduce()
 // 4. How many years did all the inventors live?
+
 const totalYears = inventors.reduce((total, inventor) => {
     return total + (inventor.passed - inventor.year);
 }, 0);
-
 // console.log(totalYears);
+
 
 // 5. Sort the inventors by years lived
 
 const oldest = inventors.sort((a, b) => {
-    const lastGuy = a.passed - a.year;
-    const nextGuy = b.passed - b.year;
-
-    return lastGuy > nextGuy ? -1 : 1;
+    const first = a.passed - a.year;
+    const next = b.passed - b.year;
+    return first > next ? 1 : -1;
 });
 // console.table(oldest);
+
 // 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
 // https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
 
@@ -57,11 +63,14 @@ const oldest = inventors.sort((a, b) => {
 // 7. sort Exercise
 // Sort the people alphabetically by last name
 
-const alpha = people.sort((lastOne, nextOne) => {
+const sortSecond = people.sort((lastOne, nextOne) => {
     const [aLast, aFirst] = lastOne.split(', ');
     const [bLast, bFirst] = nextOne.split(', ');
-    return aLast > bLast ? 1 : -1;
+    return aLast > bLast ? -1 : 1;
 });
+console.log(sortSecond);
+
+
 // console.log(alpha);
 // 8. Reduce Exercise
 // Sum up the instances of each of these
